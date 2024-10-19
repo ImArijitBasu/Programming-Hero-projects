489e43b7684938444e256f1e2994b9bdcf182d7a
Arijit <arijitbasubd@gmail.com>
2024-10-19 23:35:30 +0600

    update

--- Milestone-7/Module-40/Countries_collection/src/Components/Countries/Countries.jsx
+++ Milestone-7/Module-40/Countries_collection/src/Components/Countries/Countries.jsx
@@ -1,31 +1,32 @@
-import { useEffect, useState } from "react"
-import Country from "../Country/Country"
-
-function Countries(){
-    
-    const [countries , setCountries] = useState([])
-
-    useEffect(()=>{
-        fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    } , [])
-    return (
-        <>
-        <div>Countries</div>
-        <div className="">
-            {countries.length}
-        </div>
-        <div className="">
-            {
-                countries.map(country=>
-                    <Country key={country.cca3} country={country}></Country>
-                )
-            }
-        </div>
-        </>
-      
-    )
+import { useEffect, useState } from "react";
+import Country from "../Country/Country";
+import "./Countries.css";
+function Countries() {
+  const [countries, setCountries] = useState([]);
+  const [countryList , setCountryList] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+  const handleList = country => {
+        console.log('country visited');
+  }
+  return (
+    <>
+      <div>Countries</div>
+      <div className="">{countries.length}</div>
+      <div className="">
+        <h2>VISITED COUNTRIES</h2>
+        <ul></ul>
+      </div>
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country key={country.cca3} country={country} handleList={handleList}></Country>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default Countries
+export default Countries;
--- Milestone-7/Module-40/Countries_collection/src/Components/Country/Country.jsx
+++ Milestone-7/Module-40/Countries_collection/src/Components/Country/Country.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
-    const {name , cca3 , flags} = country;
+const Country = ({country, handleList}) => {
+    const {name , cca3 , flags , population , area} = country;
     const {png} = flags
+
+    const [visit , setVisit] = useState(false);
+    const visitButton = () => {
+        // setVisit(true)
+        setVisit(!visit)
+    }
+
     return (
-        <div className="country">
+        <div className={`country  ${ visit ? 'background' : 'none'}`}>
             <h2>Name: {name?.common}</h2>
             <h2>key: {cca3}</h2>
             <img src={png} alt="" />
+            <h3>Population: {population}</h3>
+            <h3>Area: {area}</h3>
+            <button onClick={handleList}>Add to LIST</button>
+            <br /> <br />
+            <button onClick={visitButton}>{ visit ? 'visited' : 'visit'}</button>
         </div>
     );
 };
