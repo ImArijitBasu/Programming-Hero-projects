a570a5b23358a70218ef6c259732971f11592419
Arijit <arijitbasubd@gmail.com>
2024-10-22 02:26:59 +0600

    bug fixed

--- Milestone-7/Module-42/knowledgeCafe/src/Components/Bookmarks/Bookmarks.jsx
+++ Milestone-7/Module-42/knowledgeCafe/src/Components/Bookmarks/Bookmarks.jsx
@@ -5,8 +5,8 @@ const Bookmarks = ({ bookmarks }) => {
   return (
     <div className="p-4 h-full rounded-md bg-slate-100">
       <div className="flex justify-center capitalize gap-3 mb-3">
-        <h1>bookmarks: </h1>
-        <p>{bookmarks.length}</p>
+        <h1 className="border-b-2 w-full">bookmarks: </h1>
+        <p className="border-b-2 w-full">{bookmarks.length}</p>
       </div>
       {bookmarks.map((bookmark) => (
         <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>
